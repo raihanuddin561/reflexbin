a3514158561f35e61db4d6980ed28edfe69bd233
touhidion <touhidul.mti@gmail.com>
2023-08-03 23:41:48 +0600

    add api initial setup

---
+++ reflexbin_api/src/test/java/com/reflexbin/reflexbin_api/ReflexbinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reflexbin.reflexbin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflexbinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
