007e974bb105508138a94f7d609b82ba932b6a1d
raihanuddin561 <raihanuddin561@gmail.com>
2023-10-02 23:02:16 +0600

    Login UT added

--- reflexbin_api/src/test/java/com/reflexbin/reflexbin_api/controller/UserControllerTest.java
+++ reflexbin_api/src/test/java/com/reflexbin/reflexbin_api/controller/UserControllerTest.java
@@ -1,32 +1,47 @@
 package com.reflexbin.reflexbin_api.controller;
 
 import com.reflexbin.reflexbin_api.constant.APIEndpoints;
+import com.reflexbin.reflexbin_api.constant.ApplicationConstants;
 import com.reflexbin.reflexbin_api.constant.enums.ResponseType;
 import com.reflexbin.reflexbin_api.dto.BaseResponse;
 import com.reflexbin.reflexbin_api.dto.request.UserRequestModel;
 import com.reflexbin.reflexbin_api.dto.response.UserResponseModel;
+import com.reflexbin.reflexbin_api.model.UserEntity;
+import com.reflexbin.reflexbin_api.repository.UserRepository;
+import com.reflexbin.reflexbin_api.security.CustomAuthFilter;
+import com.reflexbin.reflexbin_api.service.JWTService;
 import com.reflexbin.reflexbin_api.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest()
+@AutoConfigureMockMvc
 class UserControllerTest {
     private final String USER_REQUEST_BODY = "{\n" +
             "    \"firstName\":\"raihan\",\n" +
@@ -38,16 +53,24 @@ class UserControllerTest {
     MockMvc mockMvc;
     @Autowired
     private WebApplicationContext context;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JWTService jwtService;
     @MockBean
     private UserServiceImpl userServiceImpl;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
     private BaseResponse baseResponse;
     private UserResponseModel userResponseModel;
+    private UserEntity userEntity;
+    private CustomAuthFilter customAuthFilter;
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
+
     }
 
     @Test
@@ -67,14 +90,71 @@ class UserControllerTest {
                 .build();
         when(userServiceImpl.createUser(any(UserRequestModel.class)))
                 .thenReturn(new ResponseEntity<>(baseResponse, HttpStatus.CREATED));
-        Object nullRes = null;
         mockMvc.perform(post(APIEndpoints.USER + APIEndpoints.USER_CREATE)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(USER_REQUEST_BODY))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.responseType").value(ResponseType.SUCCESS.name()))
                 .andExpect(jsonPath("$.result.email").value(userResponseModel.getEmail()))
-                .andExpect(jsonPath("$.error").value(nullRes));
+                .andExpect(jsonPath("$.error").value((Object) null));
+    }
+
+    @Test
+    @DisplayName("Test login with valid user info")
+    public void testLoginWithValidUser_thenReturnToken() throws Exception {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.addAll(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        userEntity = UserEntity.builder()
+                .password("abc")
+                .email("raihan@gmail.com")
+                .lastName("uddin")
+                .active(true)
+                .build();
+        when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
+        when(userServiceImpl.getUserByEmail(anyString())).thenReturn(userEntity);
+        when(userServiceImpl.loadUserByUsername(anyString())).thenReturn(
+                new User(userEntity.getEmail(), userEntity.getPassword(), true,
+                        true, true, true, authorities));
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"email\":\"raihan@gmail.com\",\n" +
+                                "    \"password\":\"abc\"\n" +
+                                "}")
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.token").exists())
+                .andExpect(jsonPath("$.responseType").value(ResponseType.SUCCESS.name()));
+    }
+
+    @Test
+    @DisplayName("Test login with invalid user info")
+    public void testLoginWithInvalidUser_thenReturnBadCredentialException() throws Exception {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.addAll(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        userEntity = UserEntity.builder()
+                .password("abc")
+                .email("raihan@gmail.com")
+                .lastName("uddin")
+                .active(true)
+                .build();
+        when(passwordEncoder.matches(anyString(), anyString())).thenReturn(false);
+        when(userServiceImpl.getUserByEmail(anyString())).thenReturn(userEntity);
+        when(userServiceImpl.loadUserByUsername(anyString())).thenReturn(
+                new User(userEntity.getEmail(), userEntity.getPassword(), true,
+                        true, true, true, authorities));
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"email\":\"raihan@gmail.com\",\n" +
+                                "    \"password\":\"abc\"\n" +
+                                "}")
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error.message")
+                        .value(ApplicationConstants.BAD_CREDENTIALS))
+                .andExpect(jsonPath("$.responseType").value(ResponseType.ERROR.name()))
+                .andExpect(jsonPath("$.result").value((Object) null))
+                .andExpect(jsonPath("$.code").value(HttpStatus.BAD_REQUEST.toString()));
+
     }
 
 }
